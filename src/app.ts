ed3a2ed2e8375a993ad23cdfe094925e7a2c7d54
AdelodunAbraham <ab.adelodun@gmail.com>
2025-02-27 00:09:11 +0100

    feat(routes) setup routers

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Request, Response } from 'express';
 import { AppDataSource } from './config/db';
 import { PORT } from './config/env';
+import routes from './routes';
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
+app.use("/api", routes);
 
 app.get('/', (req: Request, res: Response) => {
   // Send a response to the client

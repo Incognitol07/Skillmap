cafb5f34e0e463d5d04675d64db6e9eae405793b
AdelodunAbraham <ab.adelodun@gmail.com>
2025-02-26 20:37:02 +0100

    feat(app) add server entry point

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,12 @@
-import { AppDataSource } from "./config/db";
+import express from 'express';
+import { AppDataSource } from './config/db';
+
+const app = express();
+app.use(express.json());
 
 AppDataSource.initialize()
-  .then(() => {
+.then(() => {
     console.log("SQLite Database connected successfully!");
+    app.listen(3000, () => console.log('Server running on http://localhost:3000'));
   })
-  .catch((error) => console.log("Error connecting to database", error));
+  .catch((error) => console.log(error));

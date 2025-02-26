072a159624e29ff19d25e8283d395d47cc8ef0b4
AdelodunAbraham <ab.adelodun@gmail.com>
2025-02-26 20:38:09 +0100

    feat(app) change port for app

--- src/app.ts
+++ src/app.ts
@@ -7,6 +7,6 @@ app.use(express.json());
 AppDataSource.initialize()
 .then(() => {
     console.log("SQLite Database connected successfully!");
-    app.listen(3000, () => console.log('Server running on http://localhost:3000'));
+    app.listen(5500, () => console.log('Server running on http://localhost:5500'));
   })
   .catch((error) => console.log(error));
